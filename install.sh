65d3a2269d5cf2e304560367777630a457250070
Aina Manoa Ratefiarison <manoaratefy@hostibox.com>
2020-08-05 12:28:32 +0300

    First release

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Creating empty log file
+touch /var/log/remote-mysql-csf.log
+
+# Creating empty CSF configuration file
+touch /etc/csf/remotemysql.allow
+
+# Copying module script
+cp ./remote-mysql-csf-add.sh /usr/local/cpanel/3rdparty/bin/remote-mysql-csf-add.sh
+cp ./remote-mysql-csf-del.sh /usr/local/cpanel/3rdparty/bin/remote-mysql-csf-del.sh
+
+# Installing hooks
+/usr/local/cpanel/bin/manage_hooks add script /usr/local/cpanel/3rdparty/bin/remote-mysql-csf-add.sh --manual --category Cpanel --event UAPI::Mysql::add_host --stage=post --escalateprivs 1
+/usr/local/cpanel/bin/manage_hooks add script /usr/local/cpanel/3rdparty/bin/remote-mysql-csf-del.sh --manual --category Cpanel --event UAPI::Mysql::delete_host --stage=post --escalateprivs 1
+
+# Import existing Remote MySQL hosts
+mysql mysql -e "select Host,User from user where Host!='localhost' group by Host;" | awk {'print "tcp:in:d=3306:s=" $1'} | sed "s/\%//g" | egrep "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | grep -v "127.0.0.1" | sort | uniq >> /etc/csf/remotemysql.allow
+
+# Remove any duplicate hosts
+cat /etc/csf/remotemysql.allow | uniq > /tmp/remotemysql.allow
+cat /tmp/remotemysql.allow > /etc/csf/remotemysql.allow
+rm -f /tmp/remotemysql.allow
+
+# Adding Remote MySQL configuration file to CSF
+echo "Include /etc/csf/remotemysql.allow" >> /etc/csf/csf.allow
+
+# Reload CSF
+csf -r
---
+++ remote-mysql-csf-add.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Configuration variables
+CSF_CONFIG_FILE="/etc/csf/remotemysql.allow"
+LOG_FILE="/var/log/remote-mysql-csf.log"
+
+TMP_FILE="$(mktemp -p /tmp custom-script-data-XXXXXXXX)"
+
+function log {
+    local date=$(date +"%D %T")
+    if [ -n "$1" ]; then
+        echo "[$date] $@" >> $LOG_FILE
+        $DEBUG && echo "[$date] $@"
+    else
+        while read data; do
+            echo "[$date] $data" >> $LOG_FILE
+            $DEBUG && echo "[$date] $data"
+        done
+    fi
+}
+
+function valid_ip()
+{
+    local  ip=$1
+    local  stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    return $stat
+}
+
+cat "${1:-/dev/stdin}" > $TMP_FILE
+
+MYSQL_HOSTNAME=$(python -c "import sys, json; print json.load(open('$TMP_FILE'))['data']['args']['host']")
+
+# Check if this is an IP
+if valid_ip $MYSQL_HOSTNAME; then
+    MYSQL_IP=$MYSQL_HOSTNAME
+else
+    # Try to convert to IP
+    MYSQL_IP=$(getent hosts $MYSQL_HOSTNAME | awk '{ print $1 }' | head -n 1)
+
+    if valid_ip $MYSQL_IP; then
+        log "$MYSQL_HOSTNAME translated to $MYSQL_IP"
+    else
+        log "Unable to convert to IP address: $MYSQL_HOSTNAME"
+        exit 1
+    fi
+fi
+
+RULE="tcp|in|d=3306|s=$MYSQL_IP"
+
+grep -qF -- "$RULE" "$CSF_CONFIG_FILE" || echo "$RULE" >> "$CSF_CONFIG_FILE"
+log "Hostname added: $MYSQL_IP"
+csf -r
+
+rm -f $TMP_FILE
