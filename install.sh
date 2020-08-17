178026e7596e5dafef4af997f2c88ae8f80489e5
RATEFIARISON Aina Manoa <manoaratefy@hostibox.com>
2020-08-17 10:49:46 +0300

    Fix rule typo
    For newer CSF version

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ chown root:root /usr/local/cpanel/3rdparty/bin/remote-mysql-csf-del.sh
 /usr/local/cpanel/bin/manage_hooks add script /usr/local/cpanel/3rdparty/bin/remote-mysql-csf-del.sh --manual --category Cpanel --event UAPI::Mysql::delete_host --stage=post --escalateprivs 1
 
 # Import existing Remote MySQL hosts
-mysql mysql -e "select Host,User from user where Host!='localhost' group by Host;" | awk {'print "tcp:in:d=3306:s=" $1'} | sed "s/\%//g" | egrep "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | grep -v "127.0.0.1" | sort | uniq >> /etc/csf/remotemysql.allow
+mysql mysql -e "select Host,User from user where Host!='localhost' group by Host;" | awk {'print "tcp|in|d=3306|s=" $1'} | sed "s/\%//g" | egrep "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | grep -v "127.0.0.1" | sort | uniq >> /etc/csf/remotemysql.allow
 
 # Remove any duplicate hosts
 cat /etc/csf/remotemysql.allow | uniq > /tmp/remotemysql.allow
